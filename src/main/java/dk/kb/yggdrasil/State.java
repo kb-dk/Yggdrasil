f1b61f5636f27d65941a7fc8605f614f06e68376
svcarlsen <svc@kb.dk>
2013-10-22 17:17:49 +0200

    Followup after review of TA855 (YGG-3)

--- src/main/java/dk/kb/yggdrasil/State.java
+++ src/main/java/dk/kb/yggdrasil/State.java
@@ -1,5 +1,6 @@
 package dk.kb.yggdrasil;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,17 +12,17 @@ import java.util.Set;
  *      or the failstate PRESERVATION_REQUEST_RECEIVED_BUT_INCOMPLETE          
  *  Step 2: If not failed yet, download metadata from Valhal. This can either go wrong (
  *      failstate PRESERVATION_METADATA_DOWNLOAD_FAILURE) or not resulting in 
- *      state PRESERVATION_METADATA_DOWNLOAD_SUCCESS
+ *      the state PRESERVATION_METADATA_DOWNLOAD_SUCCESS
  *  Step 3: If metadata download succesfully, package the metadata . This can result in either failstate 
  *      PRESERVATION_METADATA_PACKAGED_FAILURE or successtate PRESERVATION_METADATA_PACKAGED_SUCCESSFULLY
  *  Step 4: If still no failstate, we continue with fetching the resources associated with this metadata.
- *       This can result in failstate PRESERVATION_RESOURCES_DOWNLOAD_FAILURE or the OK-state PRESERVATION_RESOURCES_DOWNLOAD_SUCCESS
+ *       This can result in the failstate PRESERVATION_RESOURCES_DOWNLOAD_FAILURE or the OK-state PRESERVATION_RESOURCES_DOWNLOAD_SUCCESS
  *  Step 5: If still good to go, we package the data in the warc-format. No failstate here (need one), 
  *      but if more data required before upload to bitrepository we go to wait-state PRESERVATION_PACKAGE_WAITING_FOR_MORE_DATA
  *      otherwise we change state to PRESERVATION_PACKAGE_COMPLETE (is this state necessary?). If in PRESERVATION_PACKAGE_COMPLETE remember to check
  *      if status for other requests can be changed from PRESERVATION_PACKAGE_WAITING_FOR_MORE_DATA to PRESERVATION_PACKAGE_COMPLETE.
- *  Step 6: Initiate upload to Bitrepository. If initiation fails, go to failstate PRESERVATION_PACKAGE_UPLOAD_FAILURE.
- *  Step 7: Wait for Bitrepository upload to complete. This can result in failstate PRESERVATION_PACKAGE_UPLOAD_FAILURE or
+ *  Step 6: Initiate upload to Bitrepository. If initiation fails, go to the failstate PRESERVATION_PACKAGE_UPLOAD_FAILURE.
+ *  Step 7: Wait for Bitrepository upload to complete. This can result in the failstate PRESERVATION_PACKAGE_UPLOAD_FAILURE or
  *      final OK-state PRESERVATION_PACKAGE_UPLOAD_SUCCESS.   
  *   
  */
@@ -39,7 +40,6 @@ public enum State {
     PRESERVATION_METADATA_PACKAGED_SUCCESSFULLY, 
     /** Metadata packaged unsuccessfully (eg. METS error or similar). Failstate. */
     PRESERVATION_METADATA_PACKAGED_FAILURE, 
-    /** Metadata packaged successfully. */
     /** Resources downloaded successfully. */
     PRESERVATION_RESOURCES_DOWNLOAD_SUCCESS,
     /** Resources downloaded unsuccessfully. Failstate. */
@@ -60,17 +60,32 @@ public enum State {
     /** Upload to Bitrepository was successful. */
     PRESERVATION_PACKAGE_UPLOAD_SUCCESS;
     
+    /** Set with the failstates in this enum class. */
+    private static final Set<State> FAIL_STATES = new HashSet<State>(Arrays.asList(PRESERVATION_PACKAGE_UPLOAD_FAILURE, 
+            PRESERVATION_METADATA_DOWNLOAD_FAILURE, 
+            PRESERVATION_METADATA_PACKAGED_FAILURE, PRESERVATION_REQUEST_RECEIVED_BUT_INCOMPLETE, 
+            PRESERVATION_RESOURCES_DOWNLOAD_FAILURE));
+    
     /** 
      * @return set of failure states. 
      */
     public static Set<State> getFailStates() {
-        Set<State> failstates = new HashSet<State>();
-        failstates.add(PRESERVATION_PACKAGE_UPLOAD_FAILURE);
-        failstates.add(PRESERVATION_METADATA_DOWNLOAD_FAILURE);
-        failstates.add(PRESERVATION_METADATA_PACKAGED_FAILURE);
-        failstates.add(PRESERVATION_REQUEST_RECEIVED_BUT_INCOMPLETE);
-        failstates.add(PRESERVATION_RESOURCES_DOWNLOAD_FAILURE);
-        return failstates;
+        return FAIL_STATES;
     }
+    
+    
+    /**
+     * Method for finding out whether the given state is a failstate or not.
+     * @param aState The given state
+     * @return true, if the given state is a failstate; otherwise it returns false.
+     */
+    public static boolean isOkState(State aState){
+      if (FAIL_STATES.contains(aState)) {
+          return false;
+      } else {
+          return true;
+      }
+    }
+    
     
 }
---
+++ src/test/java/dk/kb/yggdrasil/StateTest.java
@@ -0,0 +1,30 @@
+package dk.kb.yggdrasil;
+
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Unit-tests for the State enum class. */
+@RunWith(JUnit4.class)
+public class StateTest {
+
+    @Test
+    public void testIsOkState() {
+        State okState = State.PRESERVATION_METADATA_DOWNLOAD_SUCCESS;
+        State failState = State.PRESERVATION_METADATA_DOWNLOAD_FAILURE;
+        Assert.assertTrue(State.isOkState(okState));
+        Assert.assertFalse(State.isOkState(failState));
+    }
+    
+    public void testGetFailStates() {
+        State okState = State.PRESERVATION_METADATA_DOWNLOAD_SUCCESS;
+        State failState = State.PRESERVATION_METADATA_DOWNLOAD_FAILURE;
+        Set<State> failstates = State.getFailStates();
+        Assert.assertFalse(failstates.contains(okState));
+        Assert.assertTrue(failstates.contains(failState));
+    }
+
+}
