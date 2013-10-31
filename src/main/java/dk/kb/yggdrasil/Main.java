720151b347f4106e3b5e2d2dc739a1a30517751d
svcarlsen <svc@kb.dk>
2013-10-31 19:10:23 +0100

    Add logging to Main program

--- src/main/java/dk/kb/yggdrasil/Main.java
+++ src/main/java/dk/kb/yggdrasil/Main.java
@@ -1,6 +1,9 @@
 package dk.kb.yggdrasil;
 
 import java.io.File;
+import java.util.logging.Logger;
+
+import dk.kb.yggdrasil.exceptions.YggdrasilException;
 
 /**
  * Main class of the Yggdrasil Preservation service. 
@@ -22,17 +25,18 @@ public class Main {
     /** Java Property to define user.home. */
     private static final String USER_HOME_PROPERTY = "user.home";
 
+    /** Logging mechanism. */
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+    
     /**
      * Main program of the Yggdrasil preservation service.
      * @param args No args is read here. Properties are used to locate confdir and the running mode.
-     * @throws Exception When unable to find a configuration directory or locate required settings files.
+     * @throws YggdrasilException When unable to find a configuration directory or locate required settings files.
      */
-    public static void main(String[] args) throws Exception {
-        System.out.println("Starting the Yggdrasil Main program");
+    public static void main(String[] args) throws YggdrasilException {
+        logger.info("Starting the Yggdrasil Main program");
 
-        System.out.println("Initialising settings ");
-        RunningMode m = RunningMode.getMode();
-        System.out.println("Running in mode '" + m.toString().toLowerCase() + "'");
+        logger.info("Initialising settings using runningmode '" + RunningMode.getMode() + "'");
         File configdir = null;
         String configDirFromProperties = System.getProperty(CONFIGURATION_DIRECTORY_PROPERTY); 
 
@@ -43,21 +47,19 @@ public class Main {
             configdir = new File(userHomeDir, "Yggdrasil/config");
         }
         if (!configdir.exists()) {
-            throw new Exception("Fatal error: The chosen configuration directory '" 
+            throw new YggdrasilException("Fatal error: The chosen configuration directory '" 
                     + configdir.getAbsolutePath() + "' does not exist. "); 
         }
-        System.out.println("Looking for configuration files in dir: " 
-                + configdir.getAbsolutePath());
+        logger.info("Looking for configuration files in dir: " + configdir.getAbsolutePath());
         for (String requiredSettingsFilename: REQUIRED_SETTINGS_FILES) {
             File reqFile = new File(configdir, requiredSettingsFilename);
             if (!reqFile.exists()) {
-                throw new Exception("Fatal error. Required configuration file '" 
+                throw new YggdrasilException("Fatal error. Required configuration file '" 
                         + reqFile.getAbsolutePath() + "' does not exist. "); 
             }
         }
-
-        System.out.println("Hello world!");
-        System.out.println("Shutting down the Yggdrasil Main program");
+        
+        logger.info("Shutting down the Yggdrasil Main program");
     }
 
 }
