fc963373229f4296ab68552ca1081cbe0b06c44d
Claus Jensen <cjen@kb.dk>
2014-08-21 16:11:35 +0200

    Returned to snapshot version after release.

--- src/main/java/dk/kb/yggdrasil/xslt/extension/Agent.java
+++ src/main/java/dk/kb/yggdrasil/xslt/extension/Agent.java
@@ -14,7 +14,7 @@ public class Agent {
     public static final String INGEST_AGENT_URL = "http://id.kb.dk/authorities/agents/kbDkYggdrasilIngest.xml";
 
     /** The current version of the agent. */
-    public static final String VERSION = "1.0.0-RC5";
+    public static final String VERSION = "1.0.0-SNAPSHOT";
 
     /** Organization ID. */
     public static final String ORGANIZATION_ID = "kbDk";
