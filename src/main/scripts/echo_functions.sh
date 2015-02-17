8bd2e8bd1fe458ae9cb59da3c68e53e4855d4263
Claus Jensen <cjen@kb.dk>
2015-02-17 14:24:03 +0100

    Updated the echo function script and added a license.

--- src/main/scripts/echo_functions.sh
+++ src/main/scripts/echo_functions.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 
 echo_success() {
-  [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
-  echo -n "["
-  [ "$BOOTUP" = "color" ] && $SETCOLOR_SUCCESS
-  echo -n $"  OK  "
-  [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
-  echo -n "]"
-  echo -ne "\r"
+  echo -n $"[ OK ]"
   return 0
 }
 
 echo_failure() {
-  [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
-  echo -n "["
-  [ "$BOOTUP" = "color" ] && $SETCOLOR_FAILURE
-  echo -n $"FAILED"
-  [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
-  echo -n "]"
-  echo -ne "\r"
+  echo -n $"[FAILED]"
   return 1
 }
-
 
