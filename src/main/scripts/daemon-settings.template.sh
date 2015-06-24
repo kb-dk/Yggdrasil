e9503f4ed5b2862741dc11f4cb943ef310b030f6
Claus Jensen <cjen@kb.dk>
2015-06-24 16:26:37 +0200

    Removed Java PermSize options from scrips as there were no longer supported in Java 8.

--- src/main/scripts/daemon-settings.template.sh
+++ src/main/scripts/daemon-settings.template.sh
@@ -7,7 +7,7 @@ YGGDRASIL_RUNNING_MODE=
 # The Java home path that should be used by Yggdrasil.
 JAVA_HOME=
 # The Java options to be used, the default is set below.
-JAVA_OPTS="-Xms256m -Xmx1024m -XX:PermSize=64M -XX:MaxPermSize=256M"
+JAVA_OPTS="-Xms256m -Xmx1024m"
 # The name of the daemon, default is yggdrasil.
 DNAM="yggdrasil"
 # The user under which the daemon should run, default is yggdrasil.
--- src/main/scripts/run.sh
+++ src/main/scripts/run.sh
@@ -3,7 +3,7 @@ ProgDir=`dirname "$0"`
 . "${ProgDir}/env.sh"
 
 if [ -z "${JAVA_OPTS}" ]; then
-  JAVA_OPTS="-Xms256m -Xmx1024m -XX:PermSize=64M -XX:MaxPermSize=256M"
+  JAVA_OPTS="-Xms256m -Xmx1024m"
 fi
 
 cd ${assembly.home.env.name.ref}
