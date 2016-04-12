c236b573414cdb3d3bb0e6377564863c6011c81b
Jonas Lindberg Frellesen <jolf@kb.dk>
2016-04-12 17:14:55 +0200

    Reactivated test, which was out-commented during debugging.

--- src/test/java/dk/kb/yggdrasil/ShutdownTest.java
+++ src/test/java/dk/kb/yggdrasil/ShutdownTest.java
@@ -26,12 +26,11 @@ public class ShutdownTest {
         Shutdown s = new Shutdown();
         s.main(new String[]{"test"}); 
         
-//        Config config = new Config();
-//        try (MQ mq = new MQ(config.getMqSettings())) {
-//            System.err.println(config.getMqSettings().getShutdownDestination());
-//            MqResponse response = mq.receiveMessageFromQueue(config.getMqSettings().getShutdownDestination());
-//            Assert.assertNotNull(response.getMessageType());
-//            Assert.assertEquals(response.getMessageType(), MQ.SHUTDOWN_MESSAGE_TYPE);
-//        }
+        Config config = new Config();
+        try (MQ mq = new MQ(config.getMqSettings())) {
+            MqResponse response = mq.receiveMessageFromQueue(config.getMqSettings().getShutdownDestination());
+            Assert.assertNotNull(response.getMessageType());
+            Assert.assertEquals(response.getMessageType(), MQ.SHUTDOWN_MESSAGE_TYPE);
+        }
     }
 }
